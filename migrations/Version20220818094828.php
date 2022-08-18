4dbfa4a33af64688d499e896abfbcf4574e04c25
Thomas Fouquert <tfouquert@gmail.com>
2022-08-18 13:22:28 +0200

    modify_form_booking

---
+++ migrations/Version20220818094828.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220818094828 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE booking ALTER accepted DROP DEFAULT');
+        $this->addSql('ALTER TABLE booking ALTER rejected DROP DEFAULT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE booking ALTER accepted SET DEFAULT false NOT NULL' );
+        $this->addSql('ALTER TABLE booking ALTER rejected SET DEFAULT false NOT NULL');
+    }
+}
--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -5,16 +5,33 @@ namespace App\Form;
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('numberPeople')
-            ->add('startDate')
-            ->add('endDate')
+            ->add('numberPeople', IntegerType::class, ['attr' => [
+              'min' => 1
+
+          ]])
+            ->add('startDate', DateType::class, [
+              'widget' => 'single_text',
+              // adds a class that can be selected in JavaScript
+              'attr' => ['class' => 'js-datepicker'],
+          ])
+            ->add('endDate', DateType::class, [
+              'widget' => 'single_text',
+              // adds a class that can be selected in JavaScript
+              'attr' => ['class' => 'js-datepicker'],
+          ])
+          ->add('accepted')
+          ->add('rejected')
 
         ;
     }
