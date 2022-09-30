fb6874dd8d90be64d5e57997bca0d81d2134051b
Thomas Fouquert <tfouquert@gmail.com>
2022-10-01 00:24:48 +0200

    aws bucket for image storage

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,17 +5,16 @@ namespace App\Controller;
 use App\Entity\Flat;
 use App\Form\FlatType;
 use App\Entity\ProductImage;
+use League\Flysystem\Filesystem;
 use App\Repository\FlatRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Aws\Credentials\CredentialProvider;
-use Aws\S3\S3Client;
-use Aws\Exception\AwsException;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
+use League\Flysystem\Visibility;
+
+
 
 class DashboardController extends AbstractController
 {
@@ -39,7 +38,7 @@ class DashboardController extends AbstractController
 
     #[Route('/dashboard/flat/new', name: 'dashboard_flat_new')]
     #[Route('/dashboard/flat/{id}', name: 'dashboard_flat_edit') ]
-    public function new_and_edit(Flat $flat = null, Request $request, EntityManagerInterface $entityManager): Response
+    public function new_and_edit(Flat $flat = null, Request $request, EntityManagerInterface $entityManager, Filesystem $filesystem): Response
     {
         if(!$flat) {
           $flat = new Flat();
@@ -57,10 +56,16 @@ class DashboardController extends AbstractController
           foreach ($images as $image) {
 
             $file = md5(uniqid()) . "." . $image->guessExtension();
-            $image->move(
-              $this->getParameter('images_directory'),
-              $file
-            );
+            // to upload localy on '%kernel.project_dir%/public/images/products'
+            // $image->move(
+            //   $this->getParameter('images_directory'),
+            //   $file
+            // );
+
+
+            $filesystem->write( $file,
+            file_get_contents($image->getPathName()),
+          ['visiblity' => Visibility::PUBLIC] );
 
 
             $productImage = new ProductImage();
@@ -84,7 +89,7 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/dashboard/flat/delete/{id}', name: 'flat_destroy')]
-    public function destroy(Flat $flat, Request $request, EntityManagerInterface $entityManager): Response
+    public function destroy(Flat $flat, Request $request, EntityManagerInterface $entityManager, Filesystem $filesystem): Response
     {
       if($this->isCsrfTokenValid("SUP". $flat->getId(), $request->get('_token')))
       {
@@ -92,7 +97,9 @@ class DashboardController extends AbstractController
         foreach ($images as $image) {
           # code...
           $name = $image->getImageName();
-          unlink($this->getParameter('images_directory'). '/' . $name);
+          $filesystem->delete($name);
+          // to delete localy
+          // unlink($this->getParameter('images_directory'). '/' . $name);
         }
         $entityManager->remove($flat);
         $entityManager->flush();
@@ -102,14 +109,16 @@ class DashboardController extends AbstractController
     }
 
     #[Route('/dashboard/flat/delete-image/{id}', name: 'image_destroy')]
-    public function deleteImageAction(ProductImage $image, Request $request, EntityManagerInterface $entityManager)
+    public function deleteImageAction(ProductImage $image, Request $request, EntityManagerInterface $entityManager, Filesystem $filesystem)
     {
         $flat = $image->getFlat();
 
       if($this->isCsrfTokenValid("SUP". $image->getId(), $request->get('_token')))
       {
         $name = $image->getImageName();
-        unlink($this->getParameter('images_directory'). '/' . $name);
+        $filesystem->delete($name);
+        // to delete localy
+        // unlink($this->getParameter('images_directory'). '/' . $name);
         $entityManager->remove($image);
         $entityManager->flush();
         $this->addFlash("success", 'image have been deleted');
