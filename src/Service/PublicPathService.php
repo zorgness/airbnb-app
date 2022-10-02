1c4a92e356a8c7ab9eab4bb4fc9188a6ffd47051
Thomas Fouquert <tfouquert@gmail.com>
2022-10-02 23:45:54 +0200

    update on css

--- src/Service/PublicPathService.php
+++ src/Service/PublicPathService.php
@@ -6,7 +6,7 @@ namespace App\Service;
 class PublicPathService
 {
 
-  const PUBLIC_PATH = 'https://my-new-airbnb-bucket.s3.eu-west-3.amazonaws.com/';
+  const PUBLIC_PATH = 'https://s3.eu-west-3.amazonaws.com/my-airbnb-clone-bucket1.1/';
 
 
 }
